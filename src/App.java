232cb7731973b151577a8cd79784f33f66114c89
Kaua <kakabuchweitz@gmail.com>
2023-03-30 16:46:55 -0300

    quarta aula

--- src/App.java
+++ src/App.java
@@ -6,12 +6,18 @@ import java.util.List;
 public class App {
     public static void main(String[] args) throws Exception {
         // fazer uma conexão HTTP e buscar os dados
-        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/MostPopularTVs.json";
-        ExtratorConteudo extrator = new ExtratorConteudoImdb();
+        //String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/MostPopularTVs.json";
+        //ExtratorConteudo extrator = new ExtratorConteudoImdb();
 
 
         //String url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&start_date=2022-06-12&end_date=2022-06-14";
-        //ExtratoraConteudo extrator = new ExtratoraConteudoNasa();
+        //ExtratorConteudo extrator = new ExtratoraConteudoNasa();
+
+
+        String url = "http://localhost:8080/linguagens";
+        ExtratorConteudo extrator = new ExtratorConteudoImdb();
+
+
         var http = new ClienteHttp();
         String json = http.buscaDados(url);
 
